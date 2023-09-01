2b26963e06f364f193aff97e39fb4e1b4e23c65f
Hiho2950 <109638489+Hiho2950@users.noreply.github.com>
2023-09-01 11:59:01 +0100

    fix invisible hitbox

--- source/android/FlxNewHitbox.hx
+++ source/android/FlxNewHitbox.hx
@@ -80,15 +80,33 @@ class FlxNewHitbox extends FlxSpriteGroup
 		buttonSpace = null;
 	}
 
+	private function createHintGraphic(Width:Int, Height:Int, Color:Int = 0xFFFFFF):BitmapData
+	{
+		var shape:Shape = new Shape();
+
+			shape.graphics.beginFill(Color);
+			shape.graphics.lineStyle(3, Color, 1);
+			shape.graphics.drawRect(0, 0, Width, Height);
+			shape.graphics.lineStyle(0, 0, 0);
+			shape.graphics.drawRect(3, 3, Width - 6, Height - 6);
+			shape.graphics.endFill();
+			shape.graphics.beginGradientFill(RADIAL, [Color, FlxColor.TRANSPARENT], [0.6, 0], [0, 255], null, null, null, 0.5);
+			shape.graphics.drawRect(3, 3, Width - 6, Height - 6);
+			shape.graphics.endFill();
+
+		var bitmap:BitmapData = new BitmapData(Width, Height, true, 0);
+		bitmap.draw(shape);
+		return bitmap;
+	}
+
 	private function createHint(X:Float, Y:Float, Width:Int, Height:Int, Color:Int = 0xFFFFFF):FlxButton
 	{
 		var hint:FlxButton = new FlxButton(X, Y);
-		hint.makeGraphic(Width, Height, 0xFFFFFF);
+		hint.makeGraphic(createHintGraphic(Width, Height, 0xFFFFFF));
 		hint.solid = false;
 		hint.immovable = true;
 		hint.scrollFactor.set();
 		hint.alpha = 0.00001;
-		hint.color = Color;
 		hint.onDown.callback = hint.onOver.callback = function()
 		{
 			if (hint.alpha != ClientPrefs.data.hitboxalpha)
