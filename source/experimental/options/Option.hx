afa92b83753c607fd19484d9f921dd058ee4b166
Hiho2950 <109638489+Hiho2950@users.noreply.github.com>
2023-09-01 16:40:24 +0100

    remove chatgpt idea

--- source/experimental/options/Option.hx
+++ source/experimental/options/Option.hx
@@ -1,144 +1,125 @@
 package experimental.options;
 
-class Option {
-    public var child: Alphabet;
-    public var text(get, set): String;
-    public var onChange: Dynamic -> Void = null;
+class Option
+{
+	public var child:Alphabet;
+	public var text(get, set):String;
+	public var onChange: Dynamic -> Void = null; //Pressed enter (on Bool type options) or pressed/held left/right (on other types)
 
-    public var type(get, default): String = 'bool';
-    public var scrollSpeed: Float = 50;
-    public var variable: String = null;
-    public var defaultValue: Dynamic = null;
+	public var type(get, default):String = 'bool'; //bool, int (or integer), float (or fl), percent, string (or str)
+	// Bool will use checkboxes
+	// Everything else will use a text
 
-    public var curOption: Int = 0;
-    public var options: Array<String> = null;
-    public var changeValue: Dynamic = 1;
-    public var minValue: Dynamic = null;
-    public var maxValue: Dynamic = null;
-    public var decimals: Int = 1;
+	public var scrollSpeed:Float = 50; //Only works on int/float, defines how fast it scrolls per second while holding left/right
+	private var variable:String = null; //Variable from ClientPrefs.hx
+	public var defaultValue:Dynamic = null;
 
-    public var displayFormat: String = '%v';
-    public var description: String = '';
-    public var name: String = 'Unknown';
-    public var barVisible: Bool = false;
-    public var barValue: Float = 0;
-    public var barText: String = "";
+	public var curOption:Int = 0; //Don't change this
+	public var options:Array<String> = null; //Only used in string type
+	public var changeValue:Dynamic = 1; //Only used in int/float/percent type, how much is changed when you PRESS
+	public var minValue:Dynamic = null; //Only used in int/float/percent type
+	public var maxValue:Dynamic = null; //Only used in int/float/percent type
+	public var decimals:Int = 1; //Only used in float/percent type
 
-    public function new(name: String, description: String = '', variable: String, type: String = 'bool', ?options: Array<String> = null) {
-        this.name = name;
-        this.description = description;
-        this.variable = variable;
-        this.type = type;
-        this.defaultValue = Reflect.getProperty(ClientPrefs.defaultData, variable);
-        this.options = options;
+	public var displayFormat:String = '%v'; //How String/Float/Percent/Int values are shown, %v = Current value, %d = Default value
+	public var description:String = '';
+	public var name:String = 'Unknown';
+    public var barVisible:Bool = false;
+    public var barValue:Float = 0;
+	public var barText:String = "";
 
-        if (defaultValue == 'null variable value') {
-            setDefaultValuesByType();
-        }
+	public function new(name:String, description:String = '', variable:String, type:String = 'bool', ?options:Array<String> = null)
+	{
+		this.name = name;
+		this.description = description;
+		this.variable = variable;
+		this.type = type;
+		this.defaultValue = Reflect.getProperty(ClientPrefs.defaultData, variable);
+		this.options = options;
 
-        if (getValue() == null) {
-            setValue(defaultValue);
-        }
+		if(defaultValue == 'null variable value')
+		{
+			switch(type)
+			{
+				case 'bool':
+					defaultValue = false;
+				case 'int' | 'float':
+					defaultValue = 0;
+				case 'percent':
+					defaultValue = 1;
+				case 'string':
+					defaultValue = '';
+					if(options.length > 0) {
+						defaultValue = options[0];
+					}
+			}
+		}
 
-        setAdditionalTypeAttributes();
+		if(getValue() == null) {
+			setValue(defaultValue);
+		}
 
-        if (type == 'string') {
-            updateCurOptionForStringType();
-        } else if (type == 'percent') {
-            setPercentTypeAttributes();
-        }
-    }
+		switch(type)
+		{
+			case 'string':
+				var num:Int = options.indexOf(getValue());
+				if(num > -1) {
+					curOption = num;
+				}
+	
+			case 'percent':
+				displayFormat = '%v%';
+				changeValue = 0.01;
+				minValue = 0;
+				maxValue = 1;
+				scrollSpeed = 0.5;
+				decimals = 2;
+		}
+	}
 
-    private function setDefaultValuesByType() {
-        switch (type) {
-            case 'bool':
-                defaultValue = false;
-            case 'int' | 'float':
-                defaultValue = 0;
-            case 'percent':
-                defaultValue = 1;
-            case 'string':
-                defaultValue = '';
-                if (options.length > 0) {
-                    defaultValue = options[0];
-                }
-            case 'functionnal':
-                defaultValue = null;
-        }
-    }
+	public function change()
+	{
+		//nothing lol
+		if(onChange != null) {
+			onChange(getValue());
+		}
+	}
 
-    private function setAdditionalTypeAttributes() {
-        switch (type) {
-            case 'percent':
-                displayFormat = '%v%';
-                changeValue = 0.01;
-                minValue = 0;
-                maxValue = 1;
-                scrollSpeed = 0.5;
-                decimals = 2;
-        }
-    }
+	public function getValue():Dynamic
+	{
+		return Reflect.getProperty(ClientPrefs.data, variable);
+	}
+	public function setValue(value:Dynamic)
+	{
+		Reflect.setProperty(ClientPrefs.data, variable, value);
+	}
 
-    private function updateCurOptionForStringType() {
-        var num: Int = options.indexOf(getValue());
-        if (num > -1) {
-            curOption = num;
-        }
-    }
+	private function get_text()
+	{
+		if(child != null) {
+			return child.text;
+		}
+		return null;
+	}
+	private function set_text(newValue:String = '')
+	{
+		if(child != null) {
+			child.text = newValue;
+		}
+		return null;
+	}
 
-    private function setPercentTypeAttributes() {
-        displayFormat = '%v%';
-        changeValue = 0.01;
-        minValue = 0;
-        maxValue = 1;
-        scrollSpeed = 0.5;
-        decimals = 2;
-    }
-
-    public function change() {
-        if (onChange != null) {
-            onChange(getValue());
-        }
-    }
-
-    public function getValue(): Dynamic {
-        if (variable != null) return Reflect.getProperty(ClientPrefs.data, variable);
-        return null;
-    }
-
-    public function setValue(value: Dynamic) {
-        if (variable != null) Reflect.setProperty(ClientPrefs.data, variable, value);
-    }
-
-    private function get_text() {
-        if (child != null) {
-            return child.text;
-        }
-        return null;
-    }
-
-    private function set_text(newValue: String = '') {
-        if (child != null) {
-            child.text = newValue;
-        }
-        return null;
-    }
-
-    private function get_type() {
-        var newValue: String = 'bool';
-        switch (type.toLowerCase().trim()) {
-            case 'int' | 'float' | 'percent' | 'string':
-                newValue = type;
-            case 'integer':
-                newValue = 'int';
-            case 'str':
-                newValue = 'string';
-            case 'fl':
-                newValue = 'float';
-            case 'functionnal':
-                newValue = 'functionnal';
-        }
-        type = newValue;
-        return type;
-    }
+	private function get_type()
+	{
+		var newValue:String = 'bool';
+		switch(type.toLowerCase().trim())
+		{
+			case 'int' | 'float' | 'percent' | 'string': newValue = type;
+			case 'integer': newValue = 'int';
+			case 'str': newValue = 'string';
+			case 'fl': newValue = 'float';
+		}
+		type = newValue;
+		return type;
+	}
 }
