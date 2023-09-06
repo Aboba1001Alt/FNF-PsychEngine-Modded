2912b0d21516bec9fc8ef8488c2cedffcae72fee
Hiho2950 <109638489+Hiho2950@users.noreply.github.com>
2023-09-06 12:32:17 +0100

    oops i had forget about that

--- source/psychlua/ReflectionFunctions.hx
+++ source/psychlua/ReflectionFunctions.hx
@@ -14,7 +14,11 @@ class ReflectionFunctions
 {
 	public static function implement(funk:FunkinLua)
 	{
-		var lua:State = funk.lua;
+		var lua:State = funk.lua;	
+		Lua_helper.add_callback(lua, "getModdedProperty", function(variable:String) {
+			if (ClientPrefs.data.moddedSaves.exists(variable)) return ClientPrefs.data.moddedSaves.get(variable);
+			return null;
+		});
 		Lua_helper.add_callback(lua, "getProperty", function(variable:String, ?allowMaps:Bool = false) {
 			if (ClientPrefs.data.oldSupport) {
 				if (variable == "camFollowPos.x") variable = "camGame.scroll.x";
