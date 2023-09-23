a371871c1f1ba677312f74dbed615d16a875e73a
Hiho2950 <109638489+Hiho2950@users.noreply.github.com>
2023-09-23 12:39:40 +0100

    remove and cancel

--- source/modcharting/ModchartMusicBeatState.hx
+++ source/modcharting/ModchartMusicBeatState.hx
@@ -4,7 +4,7 @@ import flixel.addons.ui.FlxUIState;
 import modcharting.*;
 import flixelwindow.Window;
 
-class ModchartMusicBeatState extends Window
+class ModchartMusicBeatState extends FlxUIState
 {
     //just stores the renderer so instancing shit can work properly i guess
  	public var playfieldRenderer:PlayfieldRenderer;
