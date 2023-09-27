91876afc10e9bea62298c0f08860ea749f21dde4
Hiho2950 <109638489+Hiho2950@users.noreply.github.com>
2023-09-27 21:51:37 +0100

    idk

--- source/experimental/online/OnlineConfig.hx
+++ source/experimental/online/OnlineConfig.hx
@@ -3,18 +3,22 @@ package experimental.online;
 import experimental.backend.InternetLoader;
 
 class OnlineConfig {
-    public var url:String = "https://raw.githubusercontent.com/Hiho2950/modsOnline/main/";
-    public var curMod:String = "";
-    public static function setUrl(user:String,repo:String) {
+    public static var url:String = "https://raw.githubusercontent.com/Hiho2950/modsOnline/main/";
+    public static var curMod:String = "";
+
+    public static function setUrl(user:String, repo:String) {
         url = "https://raw.githubusercontent.com/" + user + "/" + repo + "/main/";
     }
+
     public static function setMod(mod:String) {
         curMod = mod;
     }
+
     public static function getModsList():String {
         var rawlist:String = InternetLoader.getTextFromUrl(url + "ModsList.txt");
         return CoolUtil.listFromString(rawlist);
     }
+
     public static function getModUrl():String {
         var returned = url + curMod + "/";
         return returned;
