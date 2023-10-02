fe8764cc06b2f12e5da37555238c86d6128ca60c
Hiho2950 <109638489+Hiho2950@users.noreply.github.com>
2023-10-02 18:49:21 +0100

    idk

--- source/experimental/online/PlayOnlineState.hx
+++ source/experimental/online/PlayOnlineState.hx
@@ -1235,7 +1235,9 @@ class PlayOnlineState extends MusicBeatState
 					gottaHitNote = !section.mustHitSection;
 				}
 
-				if (client.uuid != client.server.clients[0].uuid && playMode == "multi")  gottaHitNote = !gottaHitNote;
+				if (playMode == "multi") {
+					if (client.uuid != client.server.clients[0].uuid) gottaHitNote = !gottaHitNote;
+				}
 
 				var oldNote:Note;
 				if (unspawnNotes.length > 0)
